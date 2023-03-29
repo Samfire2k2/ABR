28de387deb5ed9e6f98ff17d3856106708d1d7d1
Samfire2k2 <samuelravard@gmail.com>
2023-03-29 22:26:05 +0200

    Recherche elt 1 export TXT

--- tb.py
+++ tb.py
@@ -48,9 +48,6 @@ def generate_T_arrays(p, max_time=180):
         T = [random.randint(1,n) for i in range(n)]
         T_arrays.append(T)
         cumulated_time += test_Creer_TB(T)
-        recherche_pire_time = test_recherche_pire(Creer_TB(T))
-        with open("temps_recherche_pire_TB.txt", "a+") as fp:
-            fp.write("n = {} pire temps : {:.5f} s\n".format(n, recherche_pire_time))
     return T_arrays
 
 def best_worst_times(T_arrays):
@@ -75,31 +72,18 @@ def test_recherche_pire(TB):
     return end_time - start_time
 
 def main():
-    # fonction principale pour générer les rapports de performance de Creer_TB
     with open("temps_CreerTB.txt", "w") as fp:
         for p in range(1, 10):
             T_arrays = generate_T_arrays(p)
             best_time, worst_time = best_worst_times(T_arrays)
-            pire_temps_recherche = 0 # initialisation de la variable
-            pire_temps_total = 0
-            nb_pires_temps = 0
-            for T in T_arrays:
+            pire_temps_recherche = 0
+            while pire_temps_recherche < worst_time:
+                T = random.choice(T_arrays)
                 TB = Creer_TB(T)
-                pire_temps = test_recherche_pire(TB)
-                fp.write("n = {} p = {}\n".format(len(T), p))
-                fp.write("Temps construction TB : {:.5f} s\n".format(test_Creer_TB(T)))
-                fp.write("Temps recherche pire : {:.5f} s\n".format(pire_temps))
-                fp.write("\n")
-                pire_temps_total += pire_temps
-                nb_pires_temps += 1
-                if pire_temps > pire_temps_recherche:
-                    pire_temps_recherche = pire_temps
-            avg_pire_temps = pire_temps_total / nb_pires_temps
-            fp.write("n = {} p = {}\n".format(len(T_arrays[0]), p))
-            fp.write("Temps au mieux : {:.5f} s\n".format(best_time))
-            fp.write("Temps au pire : {:.5f} s\n".format(worst_time))
-            fp.write("Temps pire recherche : {:.5f} s\n".format(pire_temps_recherche))
-            fp.write("Temps moyen recherche pire : {:.5f} s\n".format(avg_pire_temps))
+                pire_temps_recherche = max(pire_temps_recherche, test_recherche_pire(TB))
+            fp.write("n = {} p = {}\n".format(len(T), p))
+            fp.write("Temps construction TB : {:.5f} s\n".format(test_Creer_TB(T)))
+            fp.write("Temps recherche pire : {:.5f} s\n".format(pire_temps_recherche))
             fp.write("\n")
 
 if __name__ == '__main__':
